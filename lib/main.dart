a021a2a5f83f3bcb03dbeb9d628b296e39e30f60
mathias.krill <mathias.krill@adesso.de>
2022-01-12 10:58:00 +0100

    Endless list, list creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:super_hero/super_hero.dart';
 
+// add to additional run tags  "--no-sound-null-safety" cause the superhero lib is unsafe.
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "My Superhero picker",
+      home: SuperHeroNameGenerator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SuperHeroNameGenerator extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _SuperHeroName();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SuperHeroName extends State<SuperHeroNameGenerator> {
+  final _superHeroNAmes = <String>[];
+  final _hearted = <String>{};
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Welcome to Superhero Picker"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body:  _buildBody(),
     );
   }
+
+  Widget _buildBody()  {
+      // Listview behavior is like it calls its item-builder every time the list scrolls.
+      // "i" is the index of the current item
+      return ListView.builder(
+          padding: EdgeInsets.all(16),
+          itemBuilder: (context, i)  {
+
+            // that every 2. element has a ------ line added to the listview.
+            if(i.isOdd) {
+              return Divider();
+            }
+
+            // ~/ does divide and round only integer.
+            int zahl = i ~/ 2;
+            int currentLenght = _superHeroNAmes.length;
+
+            // in first un index is 0 and length is 0 => this leads to adding more values.
+            if(zahl >= currentLenght) {
+              _superHeroNAmes.addAll(_generateSuperhHeroNames());
+            }
+
+            // returns the current item
+            return _buildRow(_superHeroNAmes[zahl]);
+          },
+      );
+  }
+
+
+  Widget _buildRow(String superHerName){
+    return ListTile(
+      title: Text(superHerName),
+    );
+
+  }
+
+  Iterable<String> _generateSuperhHeroNames() {
+
+    //fills a list with size of "10" , each position is filled with a new value which comes from a function.
+    List<String> myNames = List.generate(10, (_) => SuperHero.random());
+
+    // very simple way:
+    // List<String> myNames = [
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    //   SuperHero.random(),
+    // ];
+    return myNames;
+  }
+
 }
+
